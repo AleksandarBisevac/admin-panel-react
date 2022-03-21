38df4febba7bd93294da88d2206dfa25a5803ba6
AleksandarBisevac <alek.bisevac@gmail.com>
2022-03-21 21:02:05 +0100

    added config, routes, basic structure and styles for New page component

--- src/components/config.js
+++ src/components/config.js
@@ -3,6 +3,23 @@ import AccountBalanceOutlinedIcon from "@mui/icons-material/AccountBalanceOutlin
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
 
+//IMPORT for -> user form icons
+import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
+import VerifiedUserOutlinedIcon from "@mui/icons-material/VerifiedUserOutlined";
+import PersonIcon from "@mui/icons-material/Person";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import BadgeOutlinedIcon from "@mui/icons-material/BadgeOutlined";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import FlagCircleOutlinedIcon from "@mui/icons-material/FlagCircleOutlined";
+
+//IMPORT for -> product form icons
+import CategoryOutlinedIcon from "@mui/icons-material/CategoryOutlined";
+import LocalOfferOutlinedIcon from "@mui/icons-material/LocalOfferOutlined";
+import WarehouseOutlinedIcon from "@mui/icons-material/WarehouseOutlined";
+import DescriptionOutlinedIcon from "@mui/icons-material/DescriptionOutlined";
+import AccessTimeOutlinedIcon from "@mui/icons-material/AccessTimeOutlined";
+import LabelOutlinedIcon from "@mui/icons-material/LabelOutlined";
+
 export const WIDGETS_TYPES = {
   USERS: "USERS",
   ORDERS: "ORDERS",
@@ -50,4 +67,107 @@ export const WIDGETS_CONFIG = {
       />
     ),
   },
+};
+
+export const USER_INPUTS = {
+  title: "Add New User",
+  layout: [
+    {
+      id: 1,
+      label: "username",
+      type: "text",
+      required: true,
+      icon: <PersonIcon className="inputIcon" />,
+    },
+    {
+      id: 2,
+      label: "email",
+      type: "email",
+      required: true,
+      icon: <AlternateEmailIcon className="inputIcon" />,
+    },
+    {
+      id: 3,
+      label: "name",
+      type: "text",
+      required: true,
+      icon: <BadgeOutlinedIcon className="inputIcon" />,
+    },
+    {
+      id: 4,
+      label: "surname",
+      type: "text",
+      required: true,
+      icon: <BadgeOutlinedIcon className="inputIcon" />,
+    },
+    {
+      id: 5,
+      label: "address",
+      type: "text",
+      required: true,
+      icon: <HomeOutlinedIcon className="inputIcon" />,
+    },
+    {
+      id: 6,
+      label: "country",
+      type: "text",
+      required: true,
+      icon: <FlagCircleOutlinedIcon className="inputIcon" />,
+    },
+    {
+      id: 7,
+      label: "password",
+      type: "text",
+      required: true,
+      icon: <VerifiedUserOutlinedIcon className="inputIcon" />,
+    },
+  ],
+};
+
+export const PRODUCT_INPUT = {
+  title: "Add New Product",
+  layout: [
+    {
+      id: "p1",
+      label: "title",
+      type: "text",
+      required: true,
+      icon: <LabelOutlinedIcon className="inputIcon" />,
+    },
+    {
+      id: "p2",
+      label: "price",
+      type: "text",
+      required: true,
+      icon: <LocalOfferOutlinedIcon className="inputIcon" />,
+    },
+    {
+      id: "p3",
+      label: "category",
+      type: "text",
+      required: true,
+      icon: <CategoryOutlinedIcon className="inputIcon" />,
+    },
+    {
+      id: "p4",
+      label: "manufacturer",
+      type: "text",
+      required: true,
+      icon: <WarehouseOutlinedIcon className="inputIcon" />,
+    },
+    {
+      id: "p5",
+      label: "description",
+      type: "text",
+      required: true,
+      icon: <DescriptionOutlinedIcon className="inputIcon" />,
+    },
+    {
+      id: "p6",
+      label: "warranty",
+      type: "text",
+      required: false,
+      icon: <AccessTimeOutlinedIcon className="inputIcon" />,
+    },
+  ],
 };
--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,7 +1,57 @@
+import React from "react";
+import imgPlaceholder from "../../assets/images/no_photo.png";
+import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 import "./new.scss";
+import isImage from "../../utils/isImage";
 
-const New = () => {
-  return <div>New</div>;
+const New = ({ inputs }) => {
+  const [file, setFile] = React.useState("");
+  console.log(file);
+
+  return (
+    <div className="createNew">
+      <div className="top cardBoxShadow">
+        <h1 className="title">{inputs.title}</h1>
+      </div>
+      <div className="bottom cardBoxShadow">
+        <div className="left">
+          <img src={file ? URL.createObjectURL(file) : imgPlaceholder} alt="" />
+        </div>
+        <div className="right">
+          <form>
+            {inputs.layout.map((item, index) => {
+              return (
+                <div className="formInput" key={item.id}>
+                  <label>{item.label}</label>
+                  <div className="inputField">
+                    <span>{item.icon}</span>
+                    <input type={item.type} required={item.required} />
+                  </div>
+                </div>
+              );
+            })}
+            <div className="formInput">
+              <label htmlFor="file" className="labelUpload">
+                <DriveFolderUploadOutlinedIcon className="icon" />
+                <span>Upload Profile Image</span>
+              </label>
+              <input
+                type="file"
+                id="file"
+                onChange={(e) =>
+                  isImage(e.target.files[0]) ? setFile(e.target.files[0]) : null
+                }
+                style={{ display: "none" }}
+              />
+            </div>
+            <div className="formInput">
+              <button>Send</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default New;
