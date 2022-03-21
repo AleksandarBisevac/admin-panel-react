7b4a27bb2310a76ec1a45b6f7d9cfddf34f29ff8
AleksandarBisevac <alek.bisevac@gmail.com>
2022-03-21 21:01:11 +0100

    added protected route, error route and component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,45 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Protected from "./pages/protected/Protected";
+import Error from "./pages/error/Error";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
 import Layout from "./layout/Layout";
+import { PRODUCT_INPUT, USER_INPUTS } from "./components/config";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Layout />}>
+          <Route
+            path="/"
+            element={
+              <Protected>
+                <Layout />
+              </Protected>
+            }
+          >
             <Route index element={<Home />} />
-            <Route path="login" element={<Login />} />
             {/* USERS ROUTES */}
-            <Route path="users">
+            <Route path="/users">
               <Route index element={<List />} />
               <Route path=":userId" element={<Single />} />
-              <Route path="new" element={<New />} />
+              <Route path="new" element={<New inputs={USER_INPUTS} />} />
+            </Route>
+            {/* PRODUCTS ROUTES */}
+            <Route path="/products">
+              <Route index element={<List />} />
+              <Route path=":productId" element={<Single />} />
+              <Route path="new" element={<New inputs={PRODUCT_INPUT} />} />
             </Route>
           </Route>
-          {/* PRODUCTS ROUTES */}
-          <Route path="products">
-            <Route index element={<List />} />
-            <Route path=":productId" element={<Single />} />
-            <Route path="new" element={<New />} />
-          </Route>
+          <Route path="/login" element={<Login />} />
+
+          <Route path="*" element={<Error />} />
         </Routes>
       </BrowserRouter>
     </div>
