a9e22e9ab2e2313e895556c60435cc24df026fc5
AleksandarBisevac <alek.bisevac@gmail.com>
2022-03-21 21:02:28 +0100

    small layout and style changes

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,9 +1,16 @@
+import { Link } from "react-router-dom";
 import DataTable from "../../components/dataTable/DataTable";
 import "./list.scss";
 
 const List = () => {
   return (
-    <div className="list cardBoxShadow">
+    <div className="list">
+      <header className="listTitle">
+        <h1>users list</h1>
+        <Link to="/users/new">
+          <span>+</span> Add New
+        </Link>
+      </header>
       <DataTable />
     </div>
   );
