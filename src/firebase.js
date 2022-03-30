03bce2a04fe6e20c0d88cb9be0a6f73427f60db7
AleksandarBisevac <alek.bisevac@gmail.com>
2022-03-30 23:03:29 +0200

    firebase login config

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_PROJECT_ID + ".firebaseapp.com",
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_PROJECT_ID + ".appspot.com",
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MSG_SENDER_ID,
+    appId:
+        "1:" +
+        process.env.REACT_APP_FIREBASE_MSG_SENDER_ID +
+        ":web:360af276b877ade8ddad0d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth();
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,58 @@
+import React from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
 import "./login.scss";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+    const navigate = useNavigate();
+
+    const [error, setError] = React.useState(false);
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in
+                const user = userCredential.user;
+                console.log(user);
+                if (error) setError(false);
+                setEmail("");
+                setPassword("");
+                navigate("/");
+                // ...
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(error);
+                setError(true);
+                // ..
+            });
+    };
+
+    return (
+        <div className="login">
+            <form onSubmit={handleLogin}>
+                <input
+                    value={email ?? ""}
+                    type="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="email"
+                />
+                <input
+                    value={password ?? ""}
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="password"
+                ></input>
+                <button type="submit">Save</button>
+                {error ? <p>Wrong Email or Password</p> : null}
+            </form>
+        </div>
+    );
 };
 
 export default Login;
