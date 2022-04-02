1c3fe6c684a9364a4c385b6314f960aa8db60385
AleksandarBisevac <alek.bisevac@gmail.com>
2022-04-02 13:34:12 +0200

    work in progress

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,22 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+// Initialize DB from firestore
+import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_PROJECT_ID + ".firebaseapp.com",
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_PROJECT_ID + ".appspot.com",
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MSG_SENDER_ID,
-    appId:
-        "1:" +
-        process.env.REACT_APP_FIREBASE_MSG_SENDER_ID +
-        ":web:360af276b877ade8ddad0d",
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_PROJECT_ID + '.firebaseapp.com',
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_PROJECT_ID + '.appspot.com',
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MSG_SENDER_ID,
+  appId:
+    '1:' +
+    process.env.REACT_APP_FIREBASE_MSG_SENDER_ID +
+    ':web:360af276b877ade8ddad0d',
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
+export const db = getFirestore(app);
 export const auth = getAuth();
--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,13 +1,15 @@
-import { Link } from "react-router-dom";
-import DataTable from "../../components/dataTable/DataTable";
-import "./list.scss";
+import { Link } from 'react-router-dom';
+import DataTable from '../../components/dataTable/DataTable';
+import './list.scss';
 
 const List = () => {
+  const path = window.location.pathname;
+
   return (
-    <div className="list">
-      <header className="listTitle">
+    <div className='list'>
+      <header className='listTitle'>
         <h1>users list</h1>
-        <Link to="/users/new">
+        <Link to={`${path}/new`}>
           <span>+</span> Add New
         </Link>
       </header>
